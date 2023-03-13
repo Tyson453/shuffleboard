ccd0f07f937eab953b7237f93791f0dcfeb1cbaf
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-03-13 07:58:37 -0500

    Create plugin and example widget

---
+++ example-plugins/test-plugin/src/main/java/com/example/theme/Poin2DWidget.java
@@ -0,0 +1,36 @@
+package com.example.theme;
+
+import edu.wpi.first.shuffleboard.api.widget.Description;
+import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
+import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+import javafx.fxml.FXML;
+
+@Description(name = "MyPoin2D", dataTypes = Double.class)
+@ParametrizedController("Point2DWidget.fxml")
+public final class Point2DWidget extends SimpleAnnotatedWidget<import edu.wpi.first.shuffleboard.api.widget.Description;
+> {
+
+  private Double xValue;
+  private Double yValue;
+
+  @FXML
+  private Pane root;
+
+  @FXML
+  private Slider xSlider;
+
+  @FXML
+  private Slider ySlider;
+
+  @Override
+  private void initialize() {
+    xSlider.valueProperty().bind(dataOrDefault.map(xValue));
+    ySlider.valueProperty().bind(dataOrDefault.map(yValue));
+  }
+
+  @Override
+  public Pane getView() {
+    return root;
+  }
+
+}
